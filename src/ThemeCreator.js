30beb5cccab11ec1a02f633b641060cfdb935ce3
Kelly de Vries <kdevries@clarity-innovations.com>
2024-12-28 21:43:11 -0800

    Style the modal buttons a smidge.

--- src/ThemeCreator.js
+++ src/ThemeCreator.js
@@ -41,22 +41,23 @@ const ThemeCreator = ({ onSave }) => {
         />
       </div>
       <div className="modal-section word-arrays">
-        <label>Add bingo square words (comma separated):</label>
+        <label>Add bingo square words (comma separated)*:</label>
         <input
           type="text"
           value={wordArrays}
           onChange={(e) => setWordArrays(e.target.value)}
         />
       </div>
+      <div className="modal-footnote">*These only last until you refresh your browser, so save them elsewhere if you want to use them again.</div>
       <div className="modal-section background-color">
-      <label>Choose a background color:</label>
+      <label>Choose a background color (#hexcode or color name okay):</label>
       <input
           type="text"
           value={backgroundColor}
           onChange={(e) => setBackgroundColor(e.target.value)}
         />
       </div>
-      <button onClick={handleSave}>Save Theme</button>
+      <button className="modal-button" onClick={handleSave}>Save Theme</button>
     </div>
   );
 };
